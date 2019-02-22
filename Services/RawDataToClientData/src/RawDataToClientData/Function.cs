2f79b991b22ed0c8a20f232b51ff0fa579b44e34
Tom Dane <tjdane@gmail.com>
2019-02-22 00:58:15 -0800

    Add methods for service

--- Services/RawDataToClientData/src/RawDataToClientData/Function.cs
+++ Services/RawDataToClientData/src/RawDataToClientData/Function.cs
@@ -2,26 +2,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 using Amazon.Lambda.Core;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace RawDataToClientData
 {
     public class Function
     {
-        
-        /// <summary>
-        /// A simple function that takes a string and does a ToUpper
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public void FunctionHandler(ILambdaContext context)
         {
-            return input?.ToUpper();
+            var rawData = ReadRawData();
+            var clientData = TransformData(rawData);
+            SaveToDatabase(clientData);
+        }
+
+        public static string ReadRawData()
+        {
+            var rawData = @"{
+                ""foo"": ""bar"", 
+                ""x"": ""y"", 
+                ""1"": ""0""
+            }";
+
+            return rawData;
+        }
+        public static string TransformData(string rawData)
+        {
+            var clientData = @"{
+                ""drone"": ""bruce"", 
+                ""lat"": ""1"", 
+                ""long"": ""1""
+            }";
+
+            return clientData;
+        }
+
+        public static void SaveToDatabase(string clientData){
+
+            Console.WriteLine("Save to Database");
         }
     }
 }
