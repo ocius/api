0925db4c43aa3d57b34549507bdda7e3e5901289
Tom Dane <tjdane@gmail.com>
2019-07-10 08:07:29 -0700

    Can insert drone data with timestamp

--- Services/XmlToJson/Function.cs
+++ Services/XmlToJson/Function.cs
@@ -2,34 +2,98 @@ using System;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Xml;
-using System.Collections.Generic;
 using Amazon.Lambda.Core;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DocumentModel;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using System.Linq;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 namespace XmlToJson
 {
     public class Function
     {
-        public async Task FunctionHandler(ILambdaContext context)
+
+        private readonly string robotsEndpoint = "https://dev.ocius.com.au/usvna/oc_server?listrobots&nodeflate";
+        private readonly string statusEndpoint = "https://dev.ocius.com.au/usvna/oc_server?mavstatus&nodeflate";
+
+        public async Task FunctionHandler()
         {
-            const string endpoint = "https://dev.ocius.com.au/usvna/oc_server?mavstatus&nodeflate";
-            var xml = await Api.GetXml(endpoint);
-            var json = Json.FromXml(xml);
-            await Database.Insert(json);
+            var droneStatus = await Api.GetXml(statusEndpoint);
+            var droneNames = await Api.GetXml(robotsEndpoint);
+
+            var statusJson = Json.FromXml(droneStatus);
+            var droneArray = GetVehiclesArray(statusJson);
+
+            var nameJson = Json.FromXml(droneNames);
+            var idToNameMapping = MapIdToName(nameJson);
+
+            foreach(var droneJson in droneArray)
+            {
+                var drone = CreateDrone(droneJson, idToNameMapping);
+                var json = JsonConvert.SerializeObject(drone);
+                await Database.InsertDrone(json, drone.Name);
+            }
+        }
+
+        private static Dictionary<string, string> MapIdToName(string nameJson)
+        {
+            var data = JsonConvert.DeserializeObject(nameJson) as JObject;
+            var response = data["Response"];
+            var robots = response["robot"];
+            var result = new Dictionary<string, string>();
+
+            foreach (var robot in robots)
+            {
+                var id = robot["sysid"].ToString();
+                var name = robot["robotid"].ToString();
+                result.Add(id, name);
+            }
+
+            return result;
+        }
+
+        private static JToken GetVehiclesArray(string rawData)
+        {
+            var json = JsonConvert.DeserializeObject(rawData) as JObject;
+            var data = json["Response"]["File"];
+            return data["Vehicle"];
+        }
+
+        private static Drone CreateDrone(JToken vehicle, IDictionary<string, string> mapping)
+        {
+            var data = vehicle["mavpos"];
+            var id = data["sysid"]?.ToString();
+            if (id is null) return new Drone("Unknown", vehicle.ToString());
+            var name = mapping[id];
+            return new Drone(name, vehicle.ToString());
+        }
+    }
+
+    public class Drone
+    {
+        public string Name { get; }
+        public string Status { get; }
+
+        public Drone(string name, string status)
+        {
+            Name = name;
+            Status = status;
         }
     }
 
     public static class Api
     {
+        private static readonly HttpClient httpClient = new HttpClient();
+
         public static async Task<string> GetXml(string endpoint)
         {
-            var httpClient = new HttpClient();
             return await httpClient.GetStringAsync(endpoint);
         }
     }
+
     public static class Json
     {
         public static string FromXml(string xml)
@@ -42,12 +106,12 @@ namespace XmlToJson
 
     public static class Database
     {
-        public async static Task Insert(string json)
+        public async static Task InsertDrone(string data, string name)
         {
             var client = new AmazonDynamoDBClient();
-            var table = Table.LoadTable(client, "RawData");
-            var drone = Document.FromJson(json);
-            drone["name"] = "Bruce";
+            var table = Table.LoadTable(client, "OciusDroneData");
+            var drone = Document.FromJson(data);
+            drone["drone"] = name;
             drone["timestamp"] = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
             await table.PutItemAsync(drone);
         }
