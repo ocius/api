86d047168088db4fbb0f6609155116d8b324d9cc
Tom Dane <tjdane@gmail.com>
2019-07-11 19:03:44 -0700

    Can query based on timestamp

---
+++ Endpoints/GetAll/Database.cs
@@ -0,0 +1,55 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ociusApi
+{
+    public static class Database
+    {
+        private static readonly AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+
+        public async static Task<string> GetByTimespan(string timespan)
+        {
+
+            var query = GetQuery(timespan);
+
+            var request = new QueryRequest
+            {
+
+                TableName = "TimeSeriesDroneData",
+                KeyConditionExpression = "#dt = :query",
+                ExpressionAttributeNames = new Dictionary<string, string>
+                {
+                    {"#dt", "Date"}
+                },
+                ExpressionAttributeValues = new Dictionary<string, AttributeValue>
+                {
+                    {":query", new AttributeValue { S = query } }
+                }
+            };
+
+            var response = await client.QueryAsync(request);
+            return response.Items.Count.ToString();
+        }
+
+        private static string GetQuery(string timeSpan)
+        {
+            if(timeSpan == "hour")
+            {
+                return DateTime.UtcNow.Date.ToShortDateString();
+            }
+
+            return "live";
+        }
+
+        //live
+        //minute
+        //hour
+        //day
+        //week
+        //year
+        //all
+    }
+}
--- Endpoints/GetAll/Function.cs
+++ Endpoints/GetAll/Function.cs
@@ -1,12 +1,7 @@
-using System.Linq;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Amazon.Lambda.Core;
-using Amazon.DynamoDBv2;
-using Amazon.DynamoDBv2.DocumentModel;
-using System;
 using Newtonsoft.Json.Linq;
-using Amazon.DynamoDBv2.Model;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
@@ -17,47 +12,14 @@ namespace ociusApi
         public async Task<ApiResponse> FunctionHandler(JObject input)
         {
             var timespan = input["timespan"];
+            return await CreateResponse(timespan);
+        }
+
+        private async Task<ApiResponse> CreateResponse(JToken timespan)
+        {
             var body = await Database.GetByTimespan(timespan.ToString());
-            var headers = new Dictionary<string, string>(){ { "Access-Control-Allow-Origin", "*" } };
-            var response = new ApiResponse(200, body, headers);
-            return response;
-        }
-    }
-
-    public static class Database
-    {
-        public async static Task<string> GetByTimespan(string timespan)
-        {
-            var client = new AmazonDynamoDBClient();
-
-            var request = new QueryRequest
-            {
-                TableName = "OciusDroneData",
-                KeyConditionExpression = "#foo > :timestamp",
-                ExpressionAttributeNames = new Dictionary<String, String> {
-                    {"#foo", "timestamp"}
-                },
-                ExpressionAttributeValues = new Dictionary<string, AttributeValue> {
-                {":timestamp", new AttributeValue { N = timespan }}}
-            };
-
-            var response = await client.QueryAsync(request);
-            return response.Items.Count.ToString();
-        }
-    }
-
-    public class ApiResponse
-    {
-        public bool isBase64Encoded = false;
-        public int statusCode { get; }
-        public string body { get; }
-        public IDictionary<string, string> headers {get;}
-
-        public ApiResponse(int statusCode, string body, IDictionary<string,string> headers)
-        {
-            this.statusCode = statusCode;
-            this.body = body;
-            this.headers = headers;
+            var headers = new Dictionary<string, string>() { { "Access-Control-Allow-Origin", "*" } };
+            return new ApiResponse(200, body, headers);
         }
     }
 }
