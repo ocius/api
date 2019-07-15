4aa5a0be7de62f93f8e878dc85f7ea38cdf247c8
Tom Dane <tjdane@gmail.com>
2019-07-15 08:25:25 -0700

    Can get 200 from API gateway

--- Endpoints/GetAll/Function.cs
+++ Endpoints/GetAll/Function.cs
@@ -3,19 +3,29 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace ociusApi
 {
+    public class LambdaRequest
+    {
+        public string Body { get; set; }
+    }
+
+    public class DroneRequest
+    {
+        public string Timespan { get; set; }
+    }
+
     public class Function
     {
-        public async Task<ApiResponse> FunctionHandler(JObject input)
+        public async Task<ApiResponse> FunctionHandler(JObject request)
         {
-            var timespan = input["timespan"];
-            var body = await Database.GetByTimespan(timespan.ToString());
-            Console.WriteLine("%%%%%%%%%% BODY " + body);
-            return CreateResponse(body);
+            var input = request.ToObject<DroneRequest>();
+            var responseBody = await Database.GetByTimespan(input.Timespan);
+            return CreateResponse(responseBody);
         }
 
         private ApiResponse CreateResponse(string body)
