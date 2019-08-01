bfb42a0380d7682f6577b1da0035e1a1be3e1b86
Tom Dane <tjdane@gmail.com>
2019-08-01 08:04:26 -0700

    Add classes to get locations

--- Endpoints/GetLocations/Function.cs
+++ Endpoints/GetLocations/Function.cs
@@ -2,26 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 using Amazon.Lambda.Core;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace GetLocations
 {
+    public class DroneRequest
+    {
+        public string Timespan { get; set; }
+    }
+
     public class Function
     {
-        
-        /// <summary>
-        /// A simple function that takes a string and does a ToUpper
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public async Task<ApiResponse> FunctionHandler(JObject request)
         {
-            return input?.ToUpper();
+            var queryString = request["queryStringParameters"];
+
+            var droneRequest = queryString.ToObject<DroneRequest>();
+
+            Console.WriteLine($"TIMESPAN: {droneRequest.Timespan}");
+
+            var responseBody = await Database.GetByTimespan(droneRequest.Timespan);
+
+            return CreateResponse(responseBody);
         }
-    }
+
+        private ApiResponse CreateResponse(string body)
+        {
+            var headers = new Dictionary<string, string>() { { "Access-Control-Allow-Origin", "*" } };
+            return new ApiResponse(200, body, headers);
+        }
 }
