b56753222b2063c8e4c8470c69ca15c20e3e8f20
Tom Dane <tjdane@gmail.com>
2019-09-18 18:55:53 -0700

    Allow saving images to s3

--- Services/GetCameraImages/src/GetCameraImages/Function.cs
+++ Services/GetCameraImages/src/GetCameraImages/Function.cs
@@ -1,6 +1,10 @@
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Amazon;
 using Amazon.Lambda.Core;
+using Amazon.S3;
+using Amazon.S3.Transfer;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
@@ -8,24 +12,42 @@ namespace GetCameraImages
 {
     public class Function
     {
-        public async Task FunctionHandler()
+        private const string bucketName = "ocius-images";
+        private const string keyName = "tomamyTest2.jpg";
+
+        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.APSoutheast2;
+        private static readonly IAmazonS3 s3Client = new AmazonS3Client(bucketRegion);
+        private readonly HttpClient client = new HttpClient();
+        private readonly TransferUtility fileTransferUtility = new TransferUtility(s3Client);
+
+        public async Task<string> FunctionHandler()
         {
-            //scrape image
+            var image = await DownloadImage();
+            await UploadImage(image);
 
-            var client = new HttpClient();
-            var response = await client.GetAsync("https://tomamy.co/amytom.jpg");
-            var bytes = await response.Content.ReadAsByteArrayAsync();
-
-            //save to s3
-
-            PutObjectRequest request = new PutObjectRequest();
-            request.BucketName = "mybucket"
-            request.ContentType = contentType;
-            request.Key = key;
-            request.InputStream = file.InputStream;
-            s3Client.PutObject(request);
             //get url
+            //url is baseUrl + folderName + keyName
+            //Key name coudl be a timestamp
+
             //add to a DB
+            //save url to DB for fetching
+
+            return $"Image {keyName} uploaded";
         }
+
+        private async Task<Stream> DownloadImage()
+        {
+            var response = await client.GetAsync("https://tomamy.co/amytom.jpg");
+            return await response.Content.ReadAsStreamAsync();
+        }
+
+        private async Task UploadImage(Stream image)
+        {
+            using (image as FileStream)
+            {
+                await fileTransferUtility.UploadAsync(image, bucketName, keyName);
+            }
+        }
+
     }
 }
