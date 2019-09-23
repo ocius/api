486cffd4327d798b4a6c68ad89cdabbfbc42095b
Tom Dane <tjdane@gmail.com>
2019-09-23 07:52:07 -0700

    Measure performance of sync approach

--- Services/GetCameraImages/src/GetCameraImages/Function.cs
+++ Services/GetCameraImages/src/GetCameraImages/Function.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -32,17 +33,31 @@ namespace GetCameraImages
 
             var result = new List<string>();
 
+            var Total = new Stopwatch();
+            Total.Start();
+
             foreach (var drone in supportedDrones)
             {
                 foreach(var camera in cameras)
                 {
+                    var Loop = new Stopwatch();
+                    Loop.Start();
+
                     var imageUrl = $"{baseUrl}{drone}%20{camera}";
                     var image = await DownloadImage(imageUrl);
                     var path = $"{drone}/{timestamp}/{camera}.jpg";
                     await UploadImage(image, path);
                     result.Add(path);
+
+                    Loop.Stop();
+                    var elapsed = Total.Elapsed;
+                    result.Add("Drone time: " + elapsed.TotalSeconds.ToString());
                 }
             }
+
+            Total.Stop();
+            var stopwatchElapsed = Total.Elapsed;
+            result.Add("Total time: " + stopwatchElapsed.TotalSeconds.ToString());
 
             return result;
         }
