6f4661e4406cfc5f7d3f635b655513f3b20d849e
Tom Dane <tjdane@gmail.com>
2019-09-23 08:34:19 -0700

    Download images in serial

--- Services/GetCameraImages/src/GetCameraImages/Function.cs
+++ Services/GetCameraImages/src/GetCameraImages/Function.cs
@@ -14,23 +14,20 @@ namespace GetCameraImages
 {
     public class Function
     {
-        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.APSoutheast2;
-        private static readonly IAmazonS3 s3Client = new AmazonS3Client(bucketRegion);
+        private static TransferUtility FileTransferUtility => CreateTransferUtility();
         private readonly HttpClient client = new HttpClient();
-        private readonly TransferUtility fileTransferUtility = new TransferUtility(s3Client);
-
-        private const string bucketName = "ocius-images";
-
 
         public async Task<List<string>> FunctionHandler()
         {
-            var supportedDrones = new List<string> { "bob", "bruce" };
-
             var baseUrl = "https://usvna.ocius.com.au/usvna/oc_server?getliveimage&camera=";
-            var cameras = new List<string> { "mast" };
             var timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-
+            var supportedDrones = new List<string> { "bob", "bruce" };
+            var cameras = new List<string> { "mast" };
             var result = new List<string>();
+
+            //Although this code could download the images in paralle, it does not
+            //This is because when performance was measured, it was faster to do in serial
+            //The server was overloaded by parallel calls, and took far longer to respond to the requests
 
             foreach (var drone in supportedDrones)
             {
@@ -47,6 +44,13 @@ namespace GetCameraImages
             return result;
         }
 
+        private static TransferUtility CreateTransferUtility()
+        {
+            var bucketRegion = RegionEndpoint.APSoutheast2;
+            var s3Client = new AmazonS3Client(bucketRegion);
+            return new TransferUtility(s3Client);
+        }
+
         private async Task<Stream> DownloadImage(string url)
         {
             var response = await client.GetAsync(url);
@@ -55,11 +59,12 @@ namespace GetCameraImages
 
         private async Task UploadImage(Stream image, string path)
         {
+            var bucketName = "ocius-images";
+
             using (image as FileStream)
             {
-                await fileTransferUtility.UploadAsync(image, bucketName, path);
+                await FileTransferUtility.UploadAsync(image, bucketName, path);
             }
         }
-
     }
 }
