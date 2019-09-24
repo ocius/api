23df4e07d9d686f02a412ec1ea97f43040889f63
Tom Dane <tjdane@gmail.com>
2019-09-24 08:07:52 -0700

    Create image class

--- Services/GetCameraImages/src/GetCameraImages/Function.cs
+++ Services/GetCameraImages/src/GetCameraImages/Function.cs
@@ -1,12 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Net.Http;
 using System.Threading.Tasks;
-using Amazon;
 using Amazon.Lambda.Core;
-using Amazon.S3;
-using Amazon.S3.Transfer;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
@@ -14,20 +9,17 @@ namespace GetCameraImages
 {
     public class Function
     {
-        private static TransferUtility FileTransferUtility => CreateTransferUtility();
-        private static readonly HttpClient client = new HttpClient();
-
         public async Task<List<string>> FunctionHandler()
         {
             var timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
-            var supportedDrones = new List<string> { "bob", "bruce" };
+            var drones = new List<string> { "bob", "bruce" };
             var cameras = new List<string> { "%20mast", "_360" };
             var result = new List<string>();
 
             //This code does not download the images in parallel because when performance was measured, it was actually slower
             //The server was overloaded by parallel calls, and total response time was slower
 
-            foreach (var drone in supportedDrones)
+            foreach (var drone in drones)
             {
                 foreach(var camera in cameras)
                 {
@@ -41,47 +33,11 @@ namespace GetCameraImages
 
         private static async Task<string> SaveImage(string drone, string camera, string timestamp)
         {
-            var baseUrl = "https://usvna.ocius.com.au/usvna/oc_server?getliveimage&camera=";
+            var image = await DroneImage.Download(drone, camera);
 
-            var imageUrl = $"{baseUrl}{drone}{camera}";
-
-            var image = await DownloadImage(imageUrl);
-
-            if (image.Length == 0) return $"{imageUrl} was not found";
-
-            var path = $"{drone}/{timestamp}/{camera}.jpg";
-
-            await UploadImage(image, path);
-
-            return $"Image saved to {path}";
-        }
-
-        private static TransferUtility CreateTransferUtility()
-        {
-            var bucketRegion = RegionEndpoint.APSoutheast2;
-            var s3Client = new AmazonS3Client(bucketRegion);
-            return new TransferUtility(s3Client);
-        }
-
-        private static async Task<Stream> DownloadImage(string imageUrl)
-        {
-            var response = await client.GetAsync(imageUrl);
-
-            var message = await response.Content.ReadAsStringAsync();
-
-            return message.Contains("Could not access file") 
-                ? Stream.Null
-                : await response.Content.ReadAsStreamAsync();
-        }
-
-        private static async Task UploadImage(Stream image, string path)
-        {
-            var bucketName = "ocius-images";
-
-            using (image as FileStream)
-            {
-                await FileTransferUtility.UploadAsync(image, bucketName, path);
-            }
+            return image.HasData
+                ? await DroneImage.Upload(image.Data, drone, camera, timestamp)
+                : $"ERROR: Could not download {image.Url}";
         }
     }
 }
---
+++ Services/GetCameraImages/src/GetCameraImages/Image.cs
@@ -0,0 +1,67 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GetCameraImages
+{
+    public class DroneImage
+    {
+        public Stream Data { get; set; }
+        public string Url { get; set; }
+        public bool HasData => Data.Length != 0;
+
+        private static TransferUtility FileTransferUtility => CreateTransferUtility();
+        private static readonly HttpClient client = new HttpClient();
+
+        public static async Task<DroneImage> Download(string drone, string camera)
+        {
+            var imageUrl = $"https://usvna.ocius.com.au/usvna/oc_server?getliveimage&camera={drone}{camera}";
+
+            var response = await client.GetAsync(imageUrl);
+
+            var message = await response.Content.ReadAsStringAsync();
+
+            return message.Contains("Could not access file")
+                ? CreateEmptyImage(imageUrl)
+                : await CreateImage(imageUrl, response);
+        }
+
+        private static DroneImage CreateEmptyImage(string imageUrl)
+        {
+            return new DroneImage { Data = Stream.Null, Url = imageUrl };
+        }
+
+        private static async Task<DroneImage> CreateImage(string imageUrl, HttpResponseMessage response)
+        {
+            var data = await response.Content.ReadAsStreamAsync();
+
+            return new DroneImage { Data = data, Url = imageUrl };
+        }
+
+        public static async Task<string> Upload(Stream image, string drone, string camera, string timestamp)
+        {
+            var prettyUrl = camera.Replace("%20", "").Replace("_", "");
+
+            var path = $"{drone}/{timestamp}/{prettyUrl}.jpg";
+
+            var bucketName = "ocius-images";
+
+            using (image as FileStream)
+            {
+                await FileTransferUtility.UploadAsync(image, bucketName, path);
+            }
+
+            return $"SUCCESS: {path}";
+        }
+
+        private static TransferUtility CreateTransferUtility()
+        {
+            var bucketRegion = RegionEndpoint.APSoutheast2;
+            var s3Client = new AmazonS3Client(bucketRegion);
+            return new TransferUtility(s3Client);
+        }
+    }
+}
