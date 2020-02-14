7ada522ba29b962caacaaf5b017f97e0c337d693
Tom Dane <tjdane@gmail.com>
2020-02-13 18:28:26 -0800

    Restructure RawData project

---
+++ Services/RawDataToClientData/RawDataToClientDataTests/DroneUtilsTests.cs
@@ -0,0 +1,28 @@
+using Newtonsoft.Json.Linq;
+using RawDataToClientData.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RawDataToClientDataTests.Models
+{
+    public class DroneUtilsTests
+    {
+        [Fact]
+        public void Foo()
+        {
+            //Arrange
+            var coord = 1512346666;
+            var token = JToken.FromObject(coord);
+            var expected = 151.2346666.ToString();
+
+            //Act
+            var actual = DroneUtils.ParseCoordinates(token);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
+
