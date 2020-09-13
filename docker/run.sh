3cc69bc58a73650fa0b5f3b5ed0051043127f591
kevinwochan <kevinwochan@gmail.com>
2020-09-13 12:50:10 +1000

    fixed run script

--- docker/run.sh
+++ docker/run.sh
@@ -2,5 +2,4 @@ if [ "$#" -ne 1 ] || ! [ -d "$1" ]; then
   echo "Usage: $0 REPO_DIRECTORY" >&2
   exit 1
 fi
-SHARED_DIRECTORY="$1"
-docker run --rm -v $SHARED_DIR:/home/ocius/api -it ocius/api_dev 
+docker run --rm -v $1:/home/ocius/api -it ocius/api_dev 
