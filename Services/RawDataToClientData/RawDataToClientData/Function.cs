d3205617973f1f17f48305ddb399baffe4b9102f
Tom Dane <tjdane@gmail.com>
2020-10-25 18:46:49 -0700

    Add logs to camera fetching

--- Services/RawDataToClientData/RawDataToClientData/Function.cs
+++ Services/RawDataToClientData/RawDataToClientData/Function.cs
@@ -3,6 +3,7 @@ using Amazon.Lambda.Core;
 using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.Lambda.DynamoDBEvents;
 using Newtonsoft.Json;
+using System;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
@@ -14,6 +15,8 @@ namespace RawDataToClientData
         {
             var cameras = await Database.GetCameras();
 
+            Console.WriteLine("Camera count: " + cameras.Count);
+
             foreach (var record in dynamoEvent.Records)
             {
                 if (record.EventName != "INSERT") continue;
@@ -21,7 +24,16 @@ namespace RawDataToClientData
                 var json = Document.FromAttributeMap(record.Dynamodb.NewImage).ToJson();
                 var drone = JsonConvert.DeserializeObject<Drone>(json);
 
-                var droneCameras = cameras.ContainsKey(drone.Name) ? cameras[drone.Name] : "";
+                Console.WriteLine("Drone name: " + drone.Name);
+
+                var cameraExists = cameras.ContainsKey(drone.Name);
+
+                Console.WriteLine("camera exists: " + cameraExists);
+
+                var droneCameras = cameraExists ? cameras[drone.Name] : "";
+
+                Console.WriteLine("Drone cameras " + droneCameras);
+
                 var droneLocation = DroneLocation.GetLocationJson(drone.Name, drone.Data);
                 var droneSensors = DroneSensors.GetSensors(drone.Name, drone.Data, droneCameras);
 
