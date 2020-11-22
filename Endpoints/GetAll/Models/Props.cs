17058a321a8b859331a97567e72cc3e5b378d26a
kevinwochan <kevinwochan@gmail.com>
2020-11-23 10:19:39 +1100

    added default props

--- Endpoints/GetAll/Models/Props.cs
+++ Endpoints/GetAll/Models/Props.cs
@@ -4,16 +4,16 @@ namespace ociusApi
 {
     public class Props
     {
-        public string Water_depth { get; set; }
-        public string Water_temp { get; set; }
-        public string Wind_speed { get; set; }
-        public string Wind_direction { get; set; }
-        public string Boat_speed { get; set; }
-        public string Heading { get; set; }
-        public List<string> Batteries { get; set; }
-        public List<string> BatteryPercentages { get; set; }
-        public List<string> Cameras { get; set; }
-        public Location Location { get; set; }
+        public string Water_depth { get; set; } = "0";
+        public string Water_temp { get; set; } = "0";
+        public string Wind_speed { get; set; } = "0";
+        public string Wind_direction { get; set; } = "0";
+        public string Boat_speed { get; set; } = "0";
+        public string Heading { get; set; } = "0";
+        public List<string> Batteries { get; set; } = new List<string>();
+        public List<string> BatteryPercentages { get; set; } = new List<string>();
+        public List<string> Cameras { get; set; } = new List<string>();
+        public Location Location { get; set; } 
     }
 
     public class Location
@@ -23,7 +23,7 @@ namespace ociusApi
 
     public class Coordinates
     {
-        public string Lat { get; set; }
-        public string Lon { get; set; }
+        public string Lat { get; set; } = "0";
+        public string Lon { get; set; } = "0";
     }
 }
